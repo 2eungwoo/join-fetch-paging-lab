5ccc5aaf62474690ad24cf3145b025f0f3458d20
swlee <swo98@naver.com>
2025-05-02 18:03:07 +0900

    project setting

---
+++ src/main/java/lab/jpa_join_fetch_paging/JpaJoinFetchPagingApplication.java
@@ -0,0 +1,13 @@
+package lab.jpa_join_fetch_paging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaJoinFetchPagingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaJoinFetchPagingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/lab/jpa_join_fetch_paging/JpaJoinFetchPagingApplicationTests.java
@@ -0,0 +1,13 @@
+package lab.jpa_join_fetch_paging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaJoinFetchPagingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
